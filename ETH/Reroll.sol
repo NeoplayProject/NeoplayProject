d453888c52c55c30d0a7d32d9ba9e3340ddb057c
NEOPLAYdev <35790408+NEOPLAYdev@users.noreply.github.com>
2018-02-05 17:02:58 -0500

    Update Reroll.sol
    Changed Constructor function, added data insertion

--- ETH/Reroll.sol
+++ ETH/Reroll.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.11;
 
-import "github.com/oraclize/ethereum-api/blob/master/oraclizeAPi_0.5.sol";
-
+//import "github.com/oraclize/ethereum-api/blob/master/oraclizeAPi_0.5.sol";
+import "browser/Oraclize.sol";
+import "browser/NeoPlay.sol";
 contract Reroll is usingOraclize{
     
     event Log(string text);
@@ -10,18 +11,31 @@ contract Reroll is usingOraclize{
     event LogBytes(bytes32 b);
     event onCallback(string result);
 
-    address houseaddr = 0xC4d57523b537cA61dD3a872650eCA0827Dd04649;
-    uint256 private randomInt;
-    uint public HouseEdge=1;
+    address houseaddr;
+    uint private randomInt;
+    uint public HouseEdge;
 
-    private mapping(address => uint) betVals;
-    private mapping(address => uint) betOdds; 
-    private mapping(address => bool) isSafe;
-    
-    function Reroll() internal payable, {
+    mapping(address => uint) private betVals;
+    mapping(address => uint) private betOdds; 
+    mapping(address => bool) private isSafe;
+    modifier bnr_checked{
+        if((msg.sender.balance > msg.value)&&(msg.value >= houseaddr.balance/8)){
+            _;
+        }
+    }
+    function Reroll() public bnr_checked{
+        houseaddr = 0xC4d57523b537cA61dD3a872650eCA0827Dd04649;
+        randomInt;
+        HouseEdge=1;
+    }
+    function addData(address addr, uint256 betvals,uint256 betodds) public {
+        betVals[addr]=betvals;
+        betOdds[addr]=betodds;
+        isSafe[addr]=true;
+    }
+    function play() public payable bnr_checked{
         address betAddr = msg.sender;
         uint betvals = betVals[msg.sender];
-        uint betodds = betvals[msg.sender];
         //---------------------------------------------
        if(betvals >= houseaddr.balance/8){
            oraclize_setNetwork(networkID_consensys);
@@ -29,26 +43,22 @@ contract Reroll is usingOraclize{
            rollit();
        }
         //----------------------------------------------
-        isSafe[addr]=false;
-    }
-    function Reroll(address addr,uint betvals,uint betodds) public bnr_checked{
-        betVals[addr]=betvals;
-        betOdds[addr]=betOdds;
-        isSafe[addr]=true;
+        isSafe[betAddr]=false;
     }
     function __callback(bytes32 myid, string result)public {
         onCallback(result);
         if (msg.sender != oraclize_cbAddress()) revert();
         randomInt = parseInt(result);
         newRandom(randomInt);
+        myid;
     }
     function update()public {
         oraclize_query("URL", "json(https://api.random.org/json-rpc/1/invoke).result.random.data.0", '\n{"jsonrpc":"2.0","method":"generateIntegers","params":{"apiKey":"45e90a83-8879-4727-a036-460be2a350aa","n":1,"min":0,"max":99,"replacement":true,"base":10},"id":1}');
     }
-    function rollit(){
+    function rollit() public payable{
         uint odds = betOdds[msg.sender];
-        uint private multiplier = 100/odds;
-        if(isSafe[msg.value]){
+        uint multiplier = 100/odds;
+        if(isSafe[msg.sender]){
             if(randomInt > odds){
                 //house wins
                 houseaddr.transfer(msg.value);
