424b1141b765c98557428ca933646c7d584de101
Neoplay <35790408+NEOPLAYdev@users.noreply.github.com>
2018-03-20 01:32:34 -0400

    Update ROLL.sol

--- ETH/ROLL.sol
+++ ETH/ROLL.sol
@@ -15,9 +15,6 @@ contract owned {
         owner = newOwner;
     }
 }
-
-//interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external;}
-
 contract TokenERC20 {
     string public name;
     string public symbol;
