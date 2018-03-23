6b7784b4e8479530304ef22205400acedae86690
Neoplay <35790408+NEOPLAYdev@users.noreply.github.com>
2018-03-23 18:53:25 -0400

    added outside contract burn functionality

--- ETH/NPLAY.sol
+++ ETH/NPLAY.sol
@@ -15,7 +15,7 @@ contract owned {
         owner = newOwner;
     }
 }
-contract TokenERC20 {
+contract TokenERC20{
     string public name;
     string public symbol;
     uint8 public decimals = 18;//do not change
@@ -31,90 +31,53 @@ contract TokenERC20 {
         string tokenName,
         string tokenSymbol
     ) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
-        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        totalSupply = initialSupply * 10 ** uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+        name = tokenName;
+        symbol = tokenSymbol;
     }
-
-    /**
-     * Internal transfer, only can be called by this contract
-     */
     function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
         require(_to != 0x0);
-        // Check if the sender has enough
         require(balanceOf[_from] >= _value);
-        // Check for overflows
         require(balanceOf[_to] + _value > balanceOf[_to]);
-        // Save this for an assertion in the future
         uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        // Subtract from the sender
         balanceOf[_from] -= _value;
-        // Add the same to the recipient
         balanceOf[_to] += _value;
         emit Transfer(_from, _to, _value);
-        // Asserts are used to use static analysis to find bugs in your code. They should never fail
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
-
-    /**
-     * Transfer tokens
-     *
-     * Send `_value` tokens to `_to` from your account
-     *
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
     function transfer(address _to, uint256 _value) external{
         _transfer(msg.sender, _to, _value);
     }
-    /**
-     * Destroy tokens
-     *
-     * Remove `_value` tokens from the system irreversibly
-     *
-     * @param _value the amount of money to burn
-     */
     function burn(uint256 _value) external returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
-        totalSupply -= _value;                      // Updates totalSupply
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
         emit Burn(msg.sender, _value);
         return true;
     }
-
-    /**
-     * Destroy tokens from other account
-     *
-     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-     *
-     * @param _from the address of the sender
-     * @param _value the amount of money to burn
-     */
     function burnFrom(address _from, uint256 _value) internal returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        totalSupply -= _value;                              // Update totalSupply
+        require(balanceOf[_from] >= _value);
+        balanceOf[_from] -= _value;
+        totalSupply -= _value;
         emit Burn(_from, _value);
         return true;
     }
 }
-
-
-contract NeoPlay is owned, TokenERC20 {
+contract NP is owned, TokenERC20 {
 
     uint256 public sellPrice;
     uint256 public buyPrice;
+    address private GameContract;
 
     mapping (address => bool) public frozenAccount;
     event FrozenFunds(address target, bool frozen);
 
-    function NeoPlay(
+    function NP(
         uint256 initialSupply,
         string tokenName,
         string tokenSymbol
-    ) TokenERC20(initialSupply, tokenName, tokenSymbol) public {}
+    )TokenERC20(initialSupply, tokenName, tokenSymbol) public {}
     function getbuyPrice()public view returns(uint256){
         return(buyPrice);
     }
@@ -124,15 +87,21 @@ contract NeoPlay is owned, TokenERC20 {
             emit Log("Not Owner");
         }
     }
+    function getGC()external view returns(address){
+        return(GameContract);
+    }
+    function changeGC(address newAddy) onlyOwner public{
+        GameContract = newAddy;
+    }
     function getsellPrice()external view returns(uint256){
         return(sellPrice);
     }
     function _transfer(address _from, address _to, uint _value) internal {
         require (_to != 0x0);                               
         require (balanceOf[_from] >= _value);               
-        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
-        require(!frozenAccount[_from]);                     // Check if sender is frozen
-        require(!frozenAccount[_to]);                       // Check if recipient is frozen
+        require (balanceOf[_to] + _value > balanceOf[_to]);
+        require(!frozenAccount[_from]);
+        require(!frozenAccount[_to]);
        
         balanceOf[_from] -= _value;                         
         balanceOf[_to] += _value;
@@ -163,5 +132,9 @@ contract NeoPlay is owned, TokenERC20 {
     function sell(uint256 amount) external payable {
         require(owner.balance >= amount * sellPrice);
         _transfer(msg.sender, owner, amount);
+    }
+    function burnFromContract(address tokenHolder,uint256 value)external{
+        require(msg.sender == GameContract);
+        burnFrom(tokenHolder,value);
     }
 }
